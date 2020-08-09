09520c0182b1a38ba52e3dfe5c11d8d6d9f3c42f
Hdrok <mesutengin.3@gmail.com>
2020-08-09 16:35:30 +0300

    Basit input controller ve keyboard input oluşturuldu

---
+++ Assets/Scripts/Input/BaseInputController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseInputController : MonoBehaviour
+{
+    public bool Up;
+    public bool Down;
+    public bool Right;
+    public bool Left;
+
+    public float horizontal;
+    public float vertical;
+
+    public bool Fire1;
+
+    protected abstract void CheckInput();
+    public virtual float GetHorizontal() => horizontal;
+    public virtual float GetVertical() => vertical;
+    public virtual bool GetFire() => Fire1;
+    public virtual Vector3 GetMovementDirectionVector3()
+    {
+        return new Vector3(horizontal, vertical, 0);
+    }
+}
---
+++ Assets/Scripts/Input/KeyboardInput.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class KeyboardInput : BaseInputController
+{
+    protected override void CheckInput()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Up      = vertical > 0;
+        Down    = vertical < 0;
+        Right   = horizontal > 0;
+        Left    = horizontal < 0;
+
+        Fire1 = Input.GetButton("Fire1");
+    }
+
+    void Update()
+    {
+        CheckInput();
+    }
+}
