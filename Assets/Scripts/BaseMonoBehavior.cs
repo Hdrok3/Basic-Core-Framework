7f0b4716fb2d3403a9dd1944e64fdc61a5b81acf
Hdrok <mesutengin.3@gmail.com>
2020-08-09 14:59:48 +0300

    Ortak kullanılacak basi BaseMonoBehavior oluşturuldu, daha bir çok işi var

---
+++ Assets/Scripts/BaseMonoBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseMonoBehavior : MonoBehaviour
+{
+    // TODO:
+    // Class ismi değiştirilecek.
+    // Zaman zaman içinde güncellenecek.
+
+    // Sık kullanılan değişkenlerin tutmak için,
+    // her defasında aynı şeyleri tekrar etmemek amaç.
+
+    public Transform myTransform;
+    public GameObject myGO;
+    public Rigidbody myBody;
+    public Rigidbody2D myBody2d;
+}
